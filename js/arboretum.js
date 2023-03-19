3e7378fa2448d250e35b208538a301d2f91e9085
Nate Hill <nathanielhill@gmail.com>
2023-03-19 14:56:38 -0400

    Update arboretum.js

--- js/arboretum.js
+++ js/arboretum.js
@@ -7,7 +7,7 @@ function setup() {
 
 function resetSketch(){
   let arbButton = createButton('redraw');
-  arbButton.position(width / 2, 120);
+  arbButton.position(width / 2, 135);
   arbButton.class('arbButtonClass');
   arbButton.mousePressed(resetSketch);
   let turtleString = "B";  
