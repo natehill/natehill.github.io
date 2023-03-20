95272ebc74e5b6ca064cce7ba2fab3b7e85c63af
Nate Hill <nathanielhill@gmail.com>
2023-03-19 20:17:41 -0400

    Update circinate.js

--- js/circinate.js
+++ js/circinate.js
@@ -12,7 +12,7 @@ function draw(){
   background('white');
   stroke('black');
   strokeWeight(1);
-  translate(windowWidth / 2, windowHeight);
+  //translate(windowWidth / 2, windowHeight);
   push();
   
   let turtleString = "B";
