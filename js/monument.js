8cf574676821b6c7d3c7ba45ccce3941349a7f84
Nate Hill <nathanielhill@gmail.com>
2023-03-20 21:39:25 -0400

    Update monument.js

--- js/monument.js
+++ js/monument.js
@@ -1,22 +1,24 @@
-let bg;
-let y = 0;
-
+// original awesome code here, much thanks https://editor.p5js.org/dcbriccetti/sketches/B27BquhuE
 function setup() {
-  // The background image must be the same size as the parameters
-  // into the createCanvas() method. In this program, the size of
-  // the image is 720x400 pixels.
-  bg = loadImage('img/Ocean.jpg');
-  createCanvas(1020, 775);
+  cnv = createCanvas(1020, 775, WEBGL);
+  cnv.id('monumentCanvas');
+  for (let i = -8; i <= 8; ++i) {
+    for (let j = -3; j <= 3; ++j) {
+      for (let k = -8; k <= 8; ++k) {
+        let x = i * 38;
+        let y = j * 38;
+        let z = k * 38;
+        push();
+        translate(x, y, z);
+        stroke(100, 100, 100, 60);
+        fill('white');
+        box(30);
+        pop();
+      }
+    }
+  }
 }
 
 function draw() {
   background(bg);
-
-  stroke(226, 204, 0);
-  line(0, y, width, y);
-
-  y++;
-  if (y > height) {
-    y = 0;
-  }
 }
