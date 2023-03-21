81e089543758052a59933f53ef0b4faa72a788cc
Nate Hill <nathanielhill@gmail.com>
2023-03-21 10:46:34 -0400

    Update monument.js

--- js/monument.js
+++ js/monument.js
@@ -1,10 +1,11 @@
 // original awesome code here, much thanks https://editor.p5js.org/dcbriccetti/sketches/B27BquhuE
+// image source https://www.flickr.com/photos/34053291@N05/6145158455
 function setup() {
   cnv = createCanvas(1020, 775, WEBGL);
   cnv.id('monumentCanvas');
   for (let i = -8; i <= 8; ++i) {
     for (let j = -3; j <= 3; ++j) {
-      for (let k = -8; k <= 8; ++k) {
+      for (let k = -3; k <= 3; ++k) {
         let x = i * 38;
         let y = j * 38;
         let z = k * 38;
